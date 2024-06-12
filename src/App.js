db05be26ab0584cb754292e3d6e54b74def52b5b
zeeshujunaid <zeeshan.junaid.developer@gmail.com>
2024-06-13 02:16:24 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css';import { render } from 'react-dom';
+import { Stage, Layer, Rect, Text } from 'react-konva';
+import Konva from 'konva';
+import React, { useState } from 'react';
 
 function App() {
+ const[list , setlist] = useState([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+
+    <Stage width={window.innerWidth} height={window.innerHeight * .9}>
+    <Layer>
+      {list .map(() => (
+        <BoxRender />
+        ))}
+    </Layer>
+  </Stage>
+  <button onClick={() => setlist(prev => [...prev,1])} style={{width:"40vw",}}>ADD MORE</button>
+      </div>
   );
 }
 
 export default App;
+
+const BoxRender = () => {
+const [color, setcolor] = useState(Konva.Util.getRandomColor())
+const [value, setvalue] = useState(Math.round(Math.random() * 1300))
+const [valuex, setvaluex] = useState(Math.round(Math.random() * 500))
+
+
+return(
+  <Rect 
+  x={value}
+  y={valuex}
+  width={200}
+  height={200}
+  shadowBlur={0}
+  fill={color}
+  onclick={()=> setcolor("white")}
+  />
+)
+}
+
